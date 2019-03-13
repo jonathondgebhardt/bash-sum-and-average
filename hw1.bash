520a7c2b3600843876409db713684513ed0914d0
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2019-03-13 10:18:28 -0400

    Add input validation.

--- hw1.bash
+++ hw1.bash
@@ -2,26 +2,9 @@
 
 # Author: Jonathon Gebhardt
 # Class: CS4900-B90
-# Instructor: John Nehrbass
+# Instructor: Dr. John Nehrbass
 # Assignment: Homework 1
-
-# Input: Any number of integer inputs accepted
-# Output: Report the sum and average of the given input
-
-# Optional:
-#   Format output
-#
-#   Validate input and provide feedback when it's invalid
-#
-#   Help
-#       ?
-#       -h
-#       -help
-#       help
-#
-#   Version
-#       -v
-#       -version
+# GitHub: https://github.com/jonathondgebhardt/bash-sum-and-average
 
 # Attempt to follow usage statement convention.
 # http://courses.cms.caltech.edu/cs11/material/general/usage.html
@@ -33,40 +16,67 @@ show_help()
     echo "  n1, n2, etc.: numbers to sum and average"
 }
 
+# Attempt to follow semantic versioning.
+# https://semver.org/
 show_version()
 {
-    echo "$0 version 0.1"
+    echo "$0 version 0.1.0"
 }
 
+# Only integer values are allowed. Report invalid input and quit if this
+# is the case.
+# https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
+validate_input()
+{
+    shouldQuit=0
+
+    for n in $numbers
+    do
+        # Check if the length of n is non-zero and do equality check on
+        # n with itself. Equality check will fail for letters and float
+        # values.
+        if [ -n "$n" -a  "$n" -eq "$n" ] 2>/dev/null; then
+            : 
+        else
+          echo "$n is not an integer" 
+          shouldQuit=1
+        fi
+    done
+
+    if [ "$shouldQuit" -gt "0" ]; then
+        exit 1
+    fi
+}
+
+# Parse arguments.
 # https://dustymabe.com/2013/05/17/easy-getopt-for-a-bash-script/
 while getopts "hv" option; do
     case $option in
     h)
         show_help 
-        exit 0;
+        exit 0
         ;;
     v)
         show_version 
-        exit 0;
+        exit 0
         ;;
     esac
 done
 
 numbers=$*
+validate_input
 
-# Validate input
-
-total=0
+sum=0
 for n in $numbers
 do
-    total=$(($total + n))
+    sum=$(($sum + n))
 done
 
-# Get average of total by redirecting to bc. 
+# Get average of sum by redirecting to bc. 
 # https://askubuntu.com/questions/229446/how-to-pass-results-of-bc-to-a-variable
-average=$(bc <<< "scale=2;$total / $#")
+average=$(bc <<< "scale=2;$sum / $#")
 
-echo "Sum:" $total
+echo "Sum:" $sum
 echo "Average:" $average
 
 exit 0;
